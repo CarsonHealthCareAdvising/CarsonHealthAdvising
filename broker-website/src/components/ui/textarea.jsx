2fd356173060c7c49b3c81cac9acc0408f0013d8
CarsonHealthCareAdvising <carsonhealthcareadvising@gmail.com>
2026-06-17 17:45:55 -0400

    Delete broker-website directory

--- broker-website/src/components/ui/textarea.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export function Textarea(props) {
-  return <textarea {...props} className={`min-h-[120px] w-full rounded-xl border border-gray-300 bg-white p-3 text-sm outline-none focus:ring-2 focus:ring-black/20 ${props.className ?? ''}`} />
-}
